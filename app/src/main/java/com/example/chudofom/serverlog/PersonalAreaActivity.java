e618dfd47c6c28368a5886d43c760cb9f9a9e0e5
Дмитрий <dimondchik@mail.ru>
2016-07-13 18:03:46 +0300

    create personal area activity

--- app/src/main/java/com/example/chudofom/serverlog/PersonalAreaActivity.java
+++ app/src/main/java/com/example/chudofom/serverlog/PersonalAreaActivity.java
@@ -1,8 +1,8 @@
 package com.example.chudofom.serverlog;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class PersonalAreaActivity extends AppCompatActivity {
@@ -11,11 +11,15 @@ public class PersonalAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_area);
-        Toolbar toolbar=(Toolbar) findViewById(R.id.account_toolbar);
-        toolbar.setTitle("Личный кабинет");
-        toolbar.setTitleTextColor(Color.WHITE);
-        toolbar.setNavigationIcon(R.drawable.ic_back);
-        toolbar.setNavigationOnClickListener(a->onBackPressed());
-
+        Toolbar toolbar = (Toolbar)findViewById(R.id.account_toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.setNavigationOnClickListener(view -> onBackPressed());
+        toolbar.inflateMenu(R.menu.personal_area_menu);
+        toolbar.setOnMenuItemClickListener(view ->
+        {
+            Intent intent = new Intent(PersonalAreaActivity.this,EditActivity.class);
+            startActivity(intent);
+            return true;
+        });
     }
 }
