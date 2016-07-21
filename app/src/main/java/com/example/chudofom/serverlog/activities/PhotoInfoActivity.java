264fecfb8d7efee0daf3c7e350b182aba2275e90
Дмитрий <dimondchik@mail.ru>
2016-07-21 17:57:30 +0300

    create layout with photo information

---
+++ app/src/main/java/com/example/chudofom/serverlog/activities/PhotoInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.chudofom.serverlog.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.chudofom.serverlog.R;
+
+public class PhotoInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_info);
+    }
+
+}
